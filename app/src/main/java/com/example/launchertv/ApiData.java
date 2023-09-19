f28f32c6cc54a8f79506758b206270f14bea62be
ryan <ryangaspri@gmail.com>
2023-09-19 20:43:19 -0300

    first commit

---
+++ app/src/main/java/com/example/launchertv/ApiData.java
@@ -0,0 +1,15 @@
+package com.example.launchertv;
+
+import java.util.List;
+
+public class ApiData {
+    private int status;
+    private String imgFundo;
+    private String logomarca;
+    private String banner1;
+    private String banner2;
+    private String banner3;
+    private String botoesBloqueados;
+    private String senhaDesbloqueio;
+    private List<AppModel> apps;
+}
---
+++ app/src/main/java/com/example/launchertv/AppModel.java
@@ -0,0 +1,12 @@
+package com.example.launchertv;
+
+import java.util.List;
+
+public class AppModel {
+    private String id;
+    private String nome;
+    private String icone;
+    private String linkApk;
+    private String pacoteAbrir;
+}
+
